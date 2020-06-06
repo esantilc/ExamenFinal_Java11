3c40b8ef9d39043c28b7ed4e4d2b01d528ab7753
Eloy Eduardo Santillán Caballero <esantilc@everis.com>
2020-06-05 22:22:20 -0500

    second commit

---
+++ developer-service/src/test/java/com/developerservice/demo/DeveloperServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developerservice.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeveloperServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ skill-service/src/test/java/com/skillservice/demo/SkillServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillservice.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
