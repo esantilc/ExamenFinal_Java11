de78066970aa12596cc564bc37e778b77c7471b3
Neil Vladimir Llique Mesia <nlliquem@everis.com>
2020-06-13 09:07:02 -0500

    cambios

--- ExamenFinal_Java11/skill-service/src/main/java/com/skillservice/dao/SkillRepository.java
+++
@@ -1,8 +0,0 @@
-package com.skillservice.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.skillservice.model.Skill;
-
-public interface SkillRepository extends JpaRepository<Skill, Long> {
-}
--- ExamenFinal_Java11/skill-service/src/main/resources/import.sql
+++
@@ -1,4 +0,0 @@
-insert into skill(label, description) values('javascript', 'Javascript language skill');
-insert into skill(label, description) values('ruby', 'Ruby language skill');
-insert into skill(label, description) values('emberjs', 'Emberjs framework');
-insert into skill(label, description) values('angularjs', 'Angularjs framework');
